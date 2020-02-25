0b9c8534d25e72a9f060026a5420ef47b5747bed
itsmeanand27111999 <60578931+itsmeanand27111999@users.noreply.github.com>
2020-02-25 14:42:12 +0530

    first p

---
+++ hacker_rank/src/hacker_rank/program.java
@@ -0,0 +1,28 @@
+package hacker_rank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.IntStream;
+import java.util.Scanner;
+public class program 
+{
+
+	private static final Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+	        int N = scanner.nextInt();
+	        IntStream stream = IntStream.range(2,6);
+			if((N%2==0) /*&& (stream == IntStream.range(6, 10))*/) 
+				{
+					System.out.println("hi");
+				}
+	        
+	    
+	}
+
+
+}
